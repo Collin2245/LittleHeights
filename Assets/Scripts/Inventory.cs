6662992253614f16ba515d6f9dce11560a33dc26
Collin2245 <49080329+Collin2245@users.noreply.github.com>
2020-09-28 13:40:55 -0500

    Initial Commit

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public static Inventory Instance { get; private set; }
+    GameObject inventoryGameObject;
+    Canvas inventoryCanvas;
+    Item testItem;
+    Item templateItem;
+    public Item[][] inventoryArray;
+    // Start is called before the first frame update
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Debug.Log("creating inventory instance");
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        inventoryGameObject = GameObject.Find("InventoryCanvas");
+        inventoryCanvas = inventoryGameObject.GetComponent<Canvas>();
+        templateItem = GameObject.Find("ItemPrefab").GetComponent<Item>();
+        templateItem.id = "Blank";
+        //Setting up blank inventory
+        inventoryArray = new Item[][]
+        {
+            new Item[] { templateItem, templateItem, templateItem, templateItem, templateItem, templateItem},
+            new Item[] { templateItem, templateItem, templateItem, templateItem, templateItem, templateItem},
+            new Item[] { templateItem, templateItem, templateItem, templateItem, templateItem, templateItem},
+            new Item[] { templateItem, templateItem, templateItem, templateItem, templateItem, templateItem},
+            new Item[] { templateItem, templateItem, templateItem, templateItem, templateItem, templateItem},
+        };
+        inventoryCanvas.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (inventoryCanvas.enabled == true)
+            {
+                inventoryCanvas.enabled = false;
+            }
+            else
+            {
+                inventoryCanvas.enabled = true;
+            }
+        }   
+    }
+
+
+    public void generateInventory()
+    {
+        for (int row = 0; row < inventoryArray.Length; row++)
+        {
+            for (int column = 0; column < inventoryArray[0].Length; column++)
+            {
+                inventoryArray[row][column].display(row, column);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string id;
+    public string type;
+    public Sprite sprite;
+    private string path;
+    public RectTransform rectTransform;
+    public Image image;
+
+
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
+        path = "Items/" + this.id ;
+        sprite = Resources.Load<Sprite>(path);
+        image.sprite = this.sprite;
+    }
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void display(float x, float y, float z = 0)
+    {
+        Vector3 location = new Vector3(x, y, z);
+        rectTransform.position = location;
+    }
+}
