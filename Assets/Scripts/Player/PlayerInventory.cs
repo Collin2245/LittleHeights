ee8fd5fcf838496d01cde5f9944f71520e598ce5
Collin2245 <49080329+Collin2245@users.noreply.github.com>
2020-10-05 13:22:50 -0500

    Aseprite importer package

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour
 {
     // Start is called before the first frame update
-    GameObject slot;
-    GameObject slot1;
+    public GameObject currentItemHolder;
+    public string currentItemId;
+    //GameObject hotbar;
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentItemHolder = GameObject.Find("CurrentItemSelector").GetComponent<currentItem>().currentSlot;
+        if(currentItemHolder.transform.childCount > 0)
+        {
+            currentItemId = currentItemHolder.GetComponentInChildren<Item>().id;
+        }
+        else
+        {
+            currentItemId = "";
+        }
         
     }
 }
