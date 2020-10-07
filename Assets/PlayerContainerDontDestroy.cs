fc33e7da009431d68e654a87ccd6b328cc10f1d8
Collin2245 <49080329+Collin2245@users.noreply.github.com>
2020-10-07 15:00:51 -0500

    Started pickup item logic

---
+++ Assets/PlayerContainerDontDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContainerDontDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static PlayerContainerDontDestroy Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Debug.Log("creating inventory instance");
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.SceneManagement;
 using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
@@ -7,9 +8,75 @@ public class PlayerInventory : MonoBehaviour
     // Start is called before the first frame update
     public GameObject currentItemHolder;
     public string currentItemId;
+    public GameObject itemHolder1;
+    public GameObject itemHolder2;
+    public GameObject itemHolder3;
+    public GameObject itemHolder4;
+    public GameObject itemHolder5;
+    public GameObject itemHolder6;
+    public GameObject itemHolder7;
+    public GameObject itemHolder8;
+    public GameObject itemHolder9;
+    public GameObject itemHolder10;
+    public GameObject itemHolder11;
+    public GameObject itemHolder12;
+    public GameObject itemHolder13;
+    public GameObject itemHolder14;
+    public GameObject itemHolder15;
+    public GameObject itemHolder16;
+    public GameObject itemHolder17;
+    public GameObject itemHolder18;
+    public GameObject itemHolder19;
+    public GameObject itemHolder20;
+    public GameObject itemHolder21;
+    public GameObject itemHolder22;
+    public GameObject itemHolder23;
+    public GameObject itemHolder24;
+    public GameObject itemHolder25;
+    public GameObject itemHolder26;
+    public GameObject itemHolder27;
+    public GameObject itemHolder28;
+    public GameObject itemHolder29;
+    public GameObject itemHolder30;
+
+    public GameObject[] itemHolders;
+    
     //GameObject hotbar;
     void Start()
     {
+        itemHolders = new GameObject[]
+        {
+            itemHolder1,
+            itemHolder2,
+            itemHolder3,
+            itemHolder4,
+            itemHolder5,
+            itemHolder6,
+            itemHolder7,
+            itemHolder8,
+            itemHolder9,
+            itemHolder10,
+            itemHolder11,
+            itemHolder12,
+            itemHolder13,
+            itemHolder14,
+            itemHolder15,
+            itemHolder16,
+            itemHolder17,
+            itemHolder18,
+            itemHolder19,
+            itemHolder20,
+            itemHolder21,
+            itemHolder22,
+            itemHolder23,
+            itemHolder24,
+            itemHolder25,
+            itemHolder26,
+            itemHolder27,
+            itemHolder28,
+            itemHolder29,
+            itemHolder30,
+        };
     }
 
     // Update is called once per frame
@@ -25,5 +92,52 @@ public class PlayerInventory : MonoBehaviour
             currentItemId = "";
         }
         
+    }
+
+    public void TryToAddItemToInventory(Item item)
+    {
+        if(TryToAddItemToExistingItem(item))
+        {
+            return;
+        }
+        TryToAddItemToEmptySlot(item);
+    }
+
+    private bool TryToAddItemToExistingItem(Item item)
+    {
+        for(int i = 0; i < this.itemHolders.Length; i++)
+        {
+            if(itemHolders[i].GetComponent<InventorySlot>().itemId == "")
+            {
+                continue;
+            }else if(itemHolders[i].GetComponent<InventorySlot>().itemId == item.id)
+            {
+                if(itemHolders[i].GetComponent<InventorySlot>().currAmount + item.currAmount <= ItemQuantities.quantityForItem[item.id])
+                {
+                    itemHolders[i].GetComponent<InventorySlot>().currAmount += item.currAmount;
+                    return true;
+                }
+                Debug.Log("Found matching, but max quantity has been reached");
+                continue;
+            }
+            else
+            {
+                continue;
+            } 
+        }
+        return false;
+    }
+
+    private void TryToAddItemToEmptySlot(Item item)
+    {
+        for (int i = 0; i < this.itemHolders.Length; i++)
+        {
+            if (itemHolders[i].GetComponent<InventorySlot>().itemId == "")
+            {
+                itemHolders[i].GetComponent<InventorySlot>().GetComponent<Item>().id = item.id;
+                itemHolders[i].GetComponent<InventorySlot>().GetComponent<Item>().currAmount = item.currAmount;
+                return;
+            }
+        }
     }
 }
