19ee08b7513110db0121c453f97391d5bb6d8eaf
Collin2245 <49080329+Collin2245@users.noreply.github.com>
2020-10-12 20:39:08 -0500

    rough item placement

---
+++ Assets/PlaceTile.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+public class PlaceTile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool isActive;
+    GameObject activeTileSelector;
+    PlayerInventory playerInventory;
+    public Item currentItem;
+
+    public Tilemap placeableItemTileMap;
+    public Tile tileToPlace;
+    GameObject ItemPrefabOnMouse;
+    Vector3Int mousePosition;
+    Vector3Int prevPos;
+    Vector3Int currPos;
+    Vector3 mousePos;
+    void Start()
+    {
+        activeTileSelector = GameObject.FindGameObjectWithTag("TileManager");
+        playerInventory = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>();
+        currentItem = playerInventory.currentItem;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isActive = activeTileSelector.GetComponent<MouseHoverScript>().isActiveArea;
+        currentItem = playerInventory.currentItem;
+        mousePosition =  placeableItemTileMap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        if(currentItem)
+        {
+            tryToPlaceTile(currentItem.id);
+        }
+        
+
+    }
+
+    bool tryToPlaceTile(string itemId)
+    {
+        
+        if(isActive)
+        {
+            Tile tile = new Tile();
+            tile.sprite = Resources.Load<Sprite>("Items/" + itemId); 
+            placeableItemTileMap.SetTile(mousePosition,tile);
+            return true;
+        }
+        return false;
+    }
+
+     void SetMouseHoverTile()
+    {
+        placeableItemTileMap.SetTile(prevPos,null);
+        placeableItemTileMap.SetTile(currPos,mouseHoverTile);
+        prevPos = currPos;
+    }
+
+    void SetAndDestroyTile()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currPos =  placeableItemTileMap.WorldToCell(mousePos);
+        if(prevPos != currPos)
+        {
+            SetMouseHoverTile();
+        }
+    }
+}
---
+++ Assets/PlaceableTile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlaceableTile : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Start is called before the first frame update
+    public Tile tileToBePlaced;
+    public Tilemap tileMapToBePlacedOn;
+}
