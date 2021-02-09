c2036f531131ede48d72b2c45d93ad2dc81f5974
Collin2245 <49080329+Collin2245@users.noreply.github.com>
2021-02-08 20:53:33 -0600

    folderChanges

--- Assets/PlacedItems/AcornScript.cs
+++ Assets/PlacedItems/AcornScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class acornScript : MonoBehaviour
+public class AcornScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
