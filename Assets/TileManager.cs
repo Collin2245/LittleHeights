4948897edd40d6c7369547ab9b23091a343a1edb
Collin2245 <49080329+Collin2245@users.noreply.github.com>
2021-02-14 09:43:10 -0600

    work on tiles

--- Assets/TileManager.cs
+++ Assets/TileManager.cs
@@ -6,15 +6,35 @@ using UnityEngine.Tilemaps;
 public class TileManager : MonoBehaviour
 {
     public Tilemap baseMap;
-    public TileBase tileToUse;
+    public Tile tile;
+    public int width;
+    public int height;
+    
     void Start()
     {
-        baseMap.BoxFill(new Vector3Int(0, 0, 0), tileToUse, 0, 0, 100, 100);
+        width = 16;
+        height = 9;
+        drawMap(width, height);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            drawMap(width, height);
+        }
+    }
+
+    void drawMap(int width, int height)
+    {
+        for(int x = 0; x<width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Debug.Log("x: " + x + " y: " + y);
+                baseMap.SetTile(new Vector3Int(x, y, 0), tile);
+            }
+        }
     }
 }
