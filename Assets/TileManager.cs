b275d465f8a7e594b606a4acf9eadcd608d9541a
Collin2245 <49080329+Collin2245@users.noreply.github.com>
2021-02-14 16:44:34 -0600

    Add basic procedural generation

--- Assets/TileManager.cs
+++ Assets/TileManager.cs
@@ -7,16 +7,16 @@ public class TileManager : MonoBehaviour
 {
     public Tilemap baseMap;
     public Tile tile;
+    public Tile tile2;
     public int width;
     public int height;
     
     void Start()
     {
-        width = 16;
-        height = 9;
+        width = 100;
+        height = 100;
         drawMap(width, height);
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -24,16 +24,31 @@ public class TileManager : MonoBehaviour
         {
             drawMap(width, height);
         }
+        if (Input.GetMouseButton(0))
+        {
+            baseMap.SetTileFlags(baseMap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition)), TileFlags.None);
+            baseMap.SetColor(baseMap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition)), Color.blue);
+        }
     }
 
     void drawMap(int width, int height)
     {
-        for(int x = 0; x<width; x++)
+        for(int y = 0; y<width; y++)
         {
-            for (int y = 0; y < height; y++)
+            for (int x = 0; x < height; x++)
             {
-                Debug.Log("x: " + x + " y: " + y);
-                baseMap.SetTile(new Vector3Int(x, y, 0), tile);
+                Vector3Int point = new Vector3Int(x, y, 0);
+                float xF = (float)x/(float)width;
+                float yF = (float)y/(float)height;
+                float perlin = Mathf.PerlinNoise(xF, yF);
+                Debug.Log(perlin);
+                if(perlin <= 0.5f)
+                {
+                    baseMap.SetTile(point, tile);
+                }else
+                {
+                    baseMap.SetTile(point, tile2);
+                }
             }
         }
     }
