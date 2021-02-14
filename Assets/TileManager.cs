698e976e15bb9c074a230a1501b5fd99a2a6ac0f
Collin2245 <49080329+Collin2245@users.noreply.github.com>
2021-02-13 23:29:42 -0600

    Remove some non needed items

---
+++ Assets/TileManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileManager : MonoBehaviour
+{
+    public Tilemap baseMap;
+    public TileBase tileToUse;
+    void Start()
+    {
+        baseMap.BoxFill(new Vector3Int(0, 0, 0), tileToUse, 0, 0, 100, 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
