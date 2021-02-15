c8f2cf84cef99675d41f1f2d37017c5c27b87f31
Collin2245 <49080329+Collin2245@users.noreply.github.com>
2021-02-15 12:43:30 -0600

    Include chunks

--- Assets/TileManager.cs
+++ Assets/TileManager.cs
@@ -11,62 +11,83 @@ public class TileManager : MonoBehaviour
     public Tile tile3;
     public Tile tile4;
     public Tile tile5;
-    public int width;
-    public int height;
+    public int startMult;
+    public int chunkSize;
     public float scale;
     public float seed;
-    Vector2 offset;
+    int i;
     void Start()
     {
-        offset = new Vector2(Random.Range(0.1f, 0.9f), Random.Range(0.1f, 0.9f));
-        seed = Random.Range(0.01f, 0.99f);
-        width = 100;
-        height = 100;
-        scale = 4500;
-        drawMap(width, height, scale);
+        startMult = 100;
+        seed = Random.Range(1f, 100000f);
+        chunkSize = 50;
+        scale = 2.5f;
+        DrawChunk(chunkSize, scale, seed,new Vector2Int(chunkSize*startMult, chunkSize*startMult));
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.transform.position = new Vector3(chunkSize * startMult +0.5f*chunkSize, chunkSize * startMult + 0.5f * chunkSize, 0);
+        Debug.Log("seed: " + seed);
     }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            seed = Random.Range(0.01f, 0.99f);
-            drawMap(width, height, scale);
+            i += chunkSize;
+
+            //seed = Random.Range(1f, 100000f);
+            DrawChunk(chunkSize, scale, seed, new Vector2Int((chunkSize * startMult)- chunkSize, (chunkSize * startMult) - chunkSize));
+            DrawChunk(chunkSize, scale, seed, new Vector2Int((chunkSize * startMult) + chunkSize, chunkSize * startMult));
+            DrawChunk(chunkSize, scale, seed, new Vector2Int((chunkSize * startMult) - chunkSize, chunkSize * startMult));
+            DrawChunk(chunkSize, scale, seed, new Vector2Int(chunkSize * startMult, (chunkSize * startMult) + chunkSize));
+            DrawChunk(chunkSize, scale, seed, new Vector2Int(chunkSize * startMult, (chunkSize * startMult) - chunkSize));
+            DrawChunk(chunkSize, scale, seed, new Vector2Int((chunkSize * startMult) + chunkSize, (chunkSize * startMult) + chunkSize));
+            DrawChunk(chunkSize, scale, seed, new Vector2Int((chunkSize * startMult) + chunkSize, (chunkSize * startMult) - chunkSize));
+            DrawChunk(chunkSize, scale, seed, new Vector2Int((chunkSize * startMult) - chunkSize, (chunkSize * startMult) + chunkSize));
+            Debug.Log("seed: " + seed);
         }
     }
 
-    void drawMap(int width, int height, float scale)
+    void DrawChunk(int chunkSize, float scale, float seed, Vector2Int chunk)
     {
-        for(int y = 0; y<width; y++)
+        for(int y = chunk.y; y< chunkSize + chunk.y; y++)
         {
-            for (int x = 0; x < height; x++)
+            for (int x = chunk.x; x < chunkSize + chunk.x; x++)
             {
                 Vector3Int point = new Vector3Int(x, y, 0);
-                float xF = ((float)x/(float)scale + offset.x);
-                float yF = ((float)y/(float)scale + offset.y);
+                float xF = (((float)x+seed)/(float)chunkSize * scale);
+                float yF = ((float)y/(float)chunkSize * scale);
                 float perlin = Mathf.PerlinNoise(xF, yF);
                 Debug.Log(perlin);
-                if(perlin <= 0.2f)
-                {
-                    baseMap.SetTile(point, tile);
-                }
-                else if(perlin > 0.2f && perlin <= 0.4f)
-                {
-                    baseMap.SetTile(point, tile2);
-                }
-                else if (perlin > 0.4f && perlin <= 0.6f)
-                {
-                    baseMap.SetTile(point, tile3);
-                }
-                else if (perlin > 0.8f && perlin <= 0.9f)
-                {
-                    baseMap.SetTile(point, tile4);
-                }
-                else if (perlin > 0.9f && perlin <= 1.0f)
-                {
-                    baseMap.SetTile(point, tile5);
-                }
+                PlaceTileWithPerlin(perlin, point);
             }
+        }
+    }
+
+    private void PlaceTileWithPerlin(float perlin, Vector3Int point)
+    {
+        if (perlin <= 0.35f)
+        {
+            baseMap.SetTile(point, tile);
+        }
+        else if (perlin > 0.35f && perlin <= 0.4f)
+        {
+            baseMap.SetTile(point, tile2);
+        }
+        else if (perlin > 0.4f && perlin <= 0.8f)
+        {
+            baseMap.SetTile(point, tile3);
+        }
+        else if (perlin > 0.8f && perlin <= 0.9f)
+        {
+            baseMap.SetTile(point, tile4);
+        }
+        else if (perlin > 0.9f)
+        {
+            baseMap.SetTile(point, tile5);
+        }
+        else
+        {
+            Debug.LogError(perlin);
         }
     }
 }
