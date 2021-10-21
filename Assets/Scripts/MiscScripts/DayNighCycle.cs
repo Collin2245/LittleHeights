cf69aeb67163a366fabd3bbd5d558b55ce7d82dd
Collin2245 <49080329+Collin2245@users.noreply.github.com>
2021-10-21 01:32:04 -0500

    add basic unfinished day night cycle

--- Assets/Scripts/MiscScripts/DayNighCycle.cs
+++ Assets/Scripts/MiscScripts/DayNighCycle.cs
@@ -8,29 +8,89 @@ using UnityEngine.Rendering.Universal;
 public class DayNighCycle : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    float dayTime;
+    [SerializeField]
+    float dayToNightTime;
+    [SerializeField]
+    float nightTime;
+    [SerializeField]
+    float nightToDayTime;
+    [SerializeField]
+    Color dayColor;
+    [SerializeField]
+    Color nightColor;
+
+    public float timeInDay;
+    public float currentTime;
+    public float currentDay;
+
     TextMeshProUGUI tmp;
     Light2D globalLight;
-    float r;
-    float g;
-    float b;
+    public Vector3 dayTimeTransition;
+    public Vector3 nightTimeTransition;
 
     void Start()
     {
+        timeInDay = dayTime + dayToNightTime + nightTime + nightToDayTime;
         tmp = GetComponent<TextMeshProUGUI>();
         globalLight = GetComponent<Light2D>();
-        r = 0;
-        g = 0;
-        b = 0;
+        dayTimeTransition = DayToNightColor(dayTime, dayToNightTime, dayColor, nightColor);
+        nightTimeTransition = DayToNightColor(dayToNightTime, dayTime, nightColor, dayColor);
+        globalLight.color = dayColor;
+        InvokeRepeating("updateLighting", 0f, 1f);
+    }
+
+    void updateLighting()
+    {
+        if(currentTime <= dayTime)
+        {
+            return;
+        }
+        else if (currentTime <= dayToNightTime)
+        {
+            //move color from dayTimeTonightTime
+            Vector3 temp = new Vector3(globalLight.color.r, globalLight.color.g, globalLight.color.b);
+            globalLight.color =  new Color(temp.x + dayTimeTransition.x, temp.y + dayTimeTransition.y, temp.z + dayTimeTransition.z);
+            return;
+        }
+        else if (currentTime <= nightTime)
+        {
+            return;
+        }
+        else if (currentTime <= nightToDayTime)
+        {
+            Vector3 temp = new Vector3(globalLight.color.r, globalLight.color.g, globalLight.color.b);
+            globalLight.color = new Color(temp.x - nightTimeTransition.x, temp.y - nightTimeTransition.y, temp.z - nightTimeTransition.z);
+            return;
+        }
+        else
+        {
+            currentTime = 0;
+        }
+    }
+       
+    Vector3 DayToNightColor(float start, float end, Color colorStart, Color colorEnd)
+    {
+        float timeframe = start - end;
+        float r = ((colorStart.r - colorEnd.r) / timeframe);
+        float g = ((colorStart.g - colorEnd.g) / timeframe);
+        float b = ((colorStart.b - colorEnd.b) / timeframe);
+        return new Vector3(r, g, b);
     }
 
     // Update is called once per frame
     void Update()
     {
         //tmp.text = globalLight.color.ToString();
-        tmp.text = "time:" + (Time.deltaTime).ToString();
-        r += Time.deltaTime;
-        g += Time.deltaTime;
-        b += Time.deltaTime;
+        currentTime += Time.deltaTime;
+        tmp.text = currentTime.ToString();
+        
         //globalLight.color = new Color(r,g,b);
+    }
+
+    private void FixedUpdate()
+    {
+        //updateLighting();
     }
 }
